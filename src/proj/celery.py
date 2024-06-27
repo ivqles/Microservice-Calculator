86aa990a92ba5fae0225b13360738e79e5b5798e
erichongg <honggeric@gmail.com>
2024-06-26 17:13:52 -0700

    Updates

--- src/proj/celery.py
+++ src/proj/celery.py
@@ -10,7 +10,11 @@ celery = Celery('proj',
 if __name__ == '__main__':
     celery.start()
 
+# Added to README file
 # Created Dockerfile and requirements.txt files for: addition, subtraction, multiplication, division
 # Created producer file:
     # tried to start flask app in this file but wouldnt work
     # wanted to test out if the workers worked (didn't test yet)
+# create test.py to see if the app runs (it doesnt run)
+    # it doesnt read proj as a folder
+# create config files to connect celery to rabbitMQ
